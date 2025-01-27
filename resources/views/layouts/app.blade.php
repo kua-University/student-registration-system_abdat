56c820431324dabf4e41d8b89f62056611d8c5eb
abdat <abdatyassin1@gmail.com>
2025-01-27 08:42:53 +0300

    Added the student-registration-system folder

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Goat Academy</title>
+    <link rel="stylesheet" href="{{asset('css/tailwindmain.css')}}" />
+</head>
+<body class="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-gray-100 font-sans antialiased">
+
+        @yield('content')
+    
+</body>
+</html>
