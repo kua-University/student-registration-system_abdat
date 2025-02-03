1a77230c9cfccc49b627b9b19e3bd0359267caf0
abdelaziz yassin <abdatyassin1@gmail.com>
2025-02-03 15:30:39 +0300

    Create UserFactory.php

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password'),
+            'role' => $this->faker->randomElement(['admin', 'student', 'teacher']),
+        ];
+    }
+
+    public function admin()
+    {
+        return $this->state([
+            'role' => 'admin',
+        ]);
+    }
+
+    public function student()
+    {
+        return $this->state([
+            'role' => 'student',
+        ]);
+    }
+
+    public function teacher()
+    {
+        return $this->state([
+            'role' => 'teacher',
+        ]);
+    }
+}
+sss
